c068e46f6714b95289ac6ad742be91038b2cc3bb
“Archana” <archana.villalba@gmail.com>
2019-04-26 22:07:53 +0200

    level 7

---
+++ katalist.js
@@ -0,0 +1,38 @@
+
+// Codewars Level 8 
+
+// Return Negative - Solution :
+
+  function makeNegative(num) {
+    if ( num > 0 ) {
+          return -num;
+      }
+      else if {
+          return num;
+      } else {
+          return 0}
+  }
+
+// String repeat
+
+function repeatStr (n, s) {
+    return s.repeat(n);
+    }
+
+// Even or Odd
+function even_or_odd(number) {
+    if (number % 2) {
+    return "Odd";} else {
+    return "Even";}
+    }
+  
+    //or
+
+ function even_or_odd(number) {
+    if (number%2 == 0) {
+      return "Even";
+    } else {
+      return "Odd";
+    }
+  }
+
