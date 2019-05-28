da52fa31d9fd4809a546ef37c79cae7a08708d36
“Archana” <archana.villalba@gmail.com>
2019-05-28 17:17:58 +0200

    new katas 0519

--- katalist.js
+++ katalist.js
@@ -36,3 +36,59 @@ function even_or_odd(number) {
     }
   }
 
+  // Sum [1+2+3....+100] var = 100
+
+  function sum (x) {
+    var start;
+    var result=0;
+    for (start = 1; start<=x;start++) {
+      result = start+result;
+    }
+    return result;
+  }
+  
+  console.log (sum (1000));
+
+  // Remove duplicates ([8,8,3,3,5]) > [8,3,5]
+
+  function removeDup (array) {
+    let result= [];
+    for (var i=0;i<array.length;i++) {
+        if (!result.includes(array[i])) {
+            result.push(array[i]);
+        }
+    }
+    return result;
+  }
+
+  const arr = removeDup([1,2,3,3,5,5,5]);
+  console.log(arr);
+
+    //or
+
+    let array_with_duplicates = ['DELHI','NEWYORK','DELHI','GOA','MUMBAI','CALIFORNIA','GOA']
+
+    function removeDuplicates(arr){
+        let unique_array = []
+        for(let i = 0;i < arr.length; i++){
+            if(unique_array.indexOf(arr[i]) == -1){
+                unique_array.push(arr[i])
+            }
+        }
+        return unique_array
+    }
+    
+    console.log(removeDuplicates(array_with_duplcates)); // [ 'DELHI', 'NEWYORK', 'GOA', 'MUMBAI', 'CALIFORNIA' ]
+
+    //Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.Return your answer as a number.
+
+    function sumMix(x){
+        const newArray = x.map(element => parseInt(element));
+        const sum = newArray.reduce((sum,element)=>sum+element);
+        return sum;
+        }; 
+
+    //solutions:
+    function sumMix(x){
+        return x.map(a => +a).reduce((a, b) => a + b);
+      }
