8c4b1e007dfc00c8c6e6715a3ec52d65634e15ac
“Archana” <archana.villalba@gmail.com>
2019-05-30 19:38:56 +0200

    katas from 300519

--- katalist.js
+++ katalist.js
@@ -92,3 +92,74 @@ function even_or_odd(number) {
     function sumMix(x){
         return x.map(a => +a).reduce((a, b) => a + b);
       }
+
+    // KATA - Remove first and last character
+    // Instructions : It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+    function removeChar(str){
+        var arr = str.slice(1);
+        var fin = arr.slice(0,-1);
+        return fin;
+        };
+
+
+    // KATA - Football game with scores such as ["3:2", "0-2", "1-1"]
+    //Count the sum of points knowing that if x>y=3pts / x=y 1pt / x<y 0pt
+
+    //Solution 1:
+    function points(games) {
+        return games.reduce((output,current)=>{
+        let x = parseInt(current[0]);
+        let y = parseInt(current[2]);
+        let value= x>y ? 3 : x===y ? 1 : 0;
+        return output+value;
+         },0)
+        }
+
+    //Solution 2:
+    function points(games) {
+        let total = 0;
+        games.map(game => {
+          if (game[0] === game[2]) {
+            total += 1;
+          } else if (game[0] > game[2]) {
+            total += 3;
+          }
+        });
+        return total;
+      }
+
+    //Solution 3
+
+    function points(games) {
+        var sum=0;
+        for (var i=0; i<games.length; ++i)
+        {
+          if (games[i][0]>games[i][2])
+            sum+=3;
+          if (games[i][0]==games[i][2])
+            sum+=1;
+        }
+        return sum;
+      }
+
+      // KATA : Simple, remove the spaces from the string, then return the resultant string.
+        function noSpace(x){
+            x = x.replace(/\s/g, '');
+            return x;
+       }
+
+       // KATA : Convert a Number to a String!
+
+       function numberToString(num) {
+        var str = num.toString();
+        return str;
+      }
+
+      //KATA : Convert string into number
+
+      var stringToNumber = function(str){
+        var string = parseInt(str);
+        return string;
+      }
+
